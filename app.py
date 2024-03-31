11755a2be754049f0f7f117af8881244b5f2ca6d
franklinlemaire <42901183+franklinlemaire@users.noreply.github.com>
2024-03-31 07:33:27 +0100

    Permuter deux visages

--- app.py
+++ app.py
@@ -4,4 +4,42 @@ Created on Sun Mar 31 03:09:44 2024
 
 @author: LENOVO
 """
+import cv2 as cv
+import sys
 
+# charger le classificateur pré-entraîné
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# charger l'image
+img = cv.imread('brad-angelina.jpg')
+img_gray=  cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# exécuter la détection de visage
+
+faces = face_cascade.detectMultiScale(img_gray, 1.1, 8)
+
+# Vérifier le nombre de visages
+if len(faces) != 2:
+    sys.exit('La photo doit avoir exactement deux visages, rééssayez...')
+
+#récupération des dimensions de chaque visage
+x1, y1, w1, h1 = faces[0]
+x2, y2, w2, h2 = faces[1]
+
+# extraction des deux visages de l'image
+
+face1 = img[y1:y1+h1, x1:x1+w1]
+face2 = img[y2:y2+h2, x2:x2+w2]
+
+#aux=face1
+# remplacer face2 par face1
+#face1=face2
+# remplacer face1 par face1
+#face2=aux
+ 
+# affichage des visages
+cv.imshow('face1', face1)
+cv.imshow('face2', face2)
+cv.waitKey(0)
+cv.destroyAllWindows()
+   
